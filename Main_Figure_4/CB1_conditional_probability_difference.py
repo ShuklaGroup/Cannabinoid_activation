4fb5a5aea488664900b2178a94ac4dab471ec136
soumajit2706 <46687074+soumajit2706@users.noreply.github.com>
2023-04-10 21:52:34 -0500

    Add files via upload

---
+++ Main_Figure_4/CB1_conditional_probability_difference.py
@@ -0,0 +1,70 @@
+import numpy as np 
+import os 
+import fnmatch 
+import pickle
+import sys
+import matplotlib as mpl
+from matplotlib import rc
+import matplotlib.pyplot as plt 
+
+hfont = {'fontname':'Helvetica'}
+rc('text', usetex=True)
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+
+def bar_plot():
+    fig, axs = plt.subplots(1,1,figsize=(14,7))
+    arr = [[i,j] for i in range(5) for j in range(i+1,5)]
+    mean = np.mean(bt_fraction_condition,axis=2)
+    std = np.std(bt_fraction_condition,axis=2) 
+    difference = np.zeros([10,200,2])
+    count = 0
+    for i in range(5):
+        for j in range(i+1,5):
+            for k in range(200):
+                difference[count,k,0] = np.abs(bt_fraction_condition[i,j,k,0]-bt_fraction_condition[i,j,k,1])
+                difference[count,k,1] = np.abs(bt_fraction_condition[j,i,k,0]-bt_fraction_condition[j,i,k,1])
+            count += 1
+    
+    difference_mean = np.mean(difference,axis=1)
+    difference_std = np.std(difference,axis=1)
+
+    x = np.arange(0,20,2)
+    width = 0.5 
+    colors = 'b'
+    axs.bar(x, difference_mean[:,0], yerr = difference_std[:,0], width= width, color = colors, align='center', ecolor='black', capsize=10, alpha=0.4, edgecolor=None, linewidth=4)
+
+    for i in range(len(x)):
+        axs.scatter(x[i] + np.random.random(difference[i,:,0].size) * width *0.5 - width*0.5 / 2, difference[i,:,0], color='black',alpha=0.25,s=2)
+
+    plt.ylim(0,1)
+    axs.set_xticklabels(['0|1', '0|2', '0|3','0|4','1|2','1|3','1|4','2|3','2|4','3|4'])
+    axs.set_xticks(x)
+    
+    axs.set_yticks([0.0,0.2,0.4,0.6,0.8,1.0])
+    axs.set_yticklabels([0.0,0.2,0.4,0.6,0.8,1.0])
+
+    plt.ylabel('$|P(F^{i}_{A}|F^{j}_{A})-P(F^{i}_{A}|F^{j}_{I})|$',**hfont,fontsize=24)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=22)
+    plt.tight_layout()
+    plt.savefig('CB1_conditional_probability_diff.png',dpi=500)
+
+if __name__=='__main__':    
+    conditions = ['Helix2M_Nloop','Ex16_distance','TG_diff_z','In36_distance','In57_distance']
+    features = ['Helix2M_Nloop','Ex16_distance','TG_diff_z','In36_distance','In57_distance']
+    bt_numbers = 200
+
+    bt_fraction_condition  = np.zeros([len(conditions),len(features),bt_numbers,2])
+
+    for i,condition in enumerate(conditions):
+        flag = list(range(5))
+        flag.pop(i)
+        filename = './CB1_'+condition+'.pkl'
+        temp = pickle.load(open(filename,'rb'))
+        count = 0
+        for j in flag:
+            bt_fraction_condition[i,j,:,:] = temp[count,:,:]
+            count += 1
+
+    bar_plot()
+    
---
+++ Main_Figure_4/CB2_TM6_movement_conditional_probability.py
@@ -0,0 +1,67 @@
+import numpy as np 
+import os 
+import fnmatch 
+import pickle
+import sys
+import matplotlib as mpl
+from matplotlib import rc
+import matplotlib.pyplot as plt 
+
+
+def dataloader(data,key):
+    if key in ['In6_chi2','TG_angle_z']:
+        return_data = data[:,CB2_dica[key]]*180/np.pi
+    else:
+        return_data = data[:,CB2_dica[key]]*10
+    
+    return return_data
+
+
+def bt_condition_probability(feature,bt_number,state):
+    totdist = []
+    totdist = pickle.load(open('../Bootstrap_files/CB2_bt_80_' + str(bt_number) + '_feature.pkl','rb'))
+    msm = pickle.load(open('../Bootstrap_files/CB2_bt_80_' + str(bt_number) + '_msm.pkl','rb'))
+    
+    txx = np.concatenate(totdist)
+ 
+    condition_matrix = dataloader(txx,condition)
+    feature_matrix = dataloader(txx,feature)
+
+    if feature in ['Ex16_distance','In57_distance','In52_distance']:
+        feature_truth_table = np.where(feature_matrix<thresolds[feature],1,0)
+    else:
+        feature_truth_table = np.where(feature_matrix>thresolds[feature],1,0)
+    
+    if state == 0:
+        condition_truth_table = np.where(condition_matrix>thresolds[condition],1,0)
+        mutual_truth_table = np.multiply(condition_truth_table,feature_truth_table)
+
+    else:
+        condition_truth_table = np.where(condition_matrix<thresolds[condition],1,0)
+        mutual_truth_table = np.multiply(condition_truth_table,feature_truth_table)
+ 
+    weights = np.concatenate(msm.trajectory_weights())
+    conditional_probability = np.dot(mutual_truth_table,weights)/np.dot(condition_truth_table,weights)
+
+    return conditional_probability
+
+
+if __name__=='__main__':
+    
+    condition = 'In36_distance'
+    features = ['Ex16_distance','In6_chi2','In52_distance','In57_distance']
+    thresolds = {'Ex16_distance':22.5,'In6_chi2':60,'In52_distance':14,'In57_distance':12,'In36_distance':13}
+    bt_numbers = 200
+
+    CB2_dica = {'Ex16_distance':0,'In6_chi2':1,'In52_distance':2,'In36_distance':3,'In57_distance':4}
+    
+    fraction_condition  = np.zeros([len(features),bt_numbers,2])
+    
+    x_label = {'Helix2M_Nloop':'N terminus movement', 'Ex16_distance': 'TM1 movement ','TG_diff_z':'TG translation ' ,'In6_chi2': 'TG rotation','In57_distance':'TM7 movement','In52_distance':'TM5 movement','In36_distance': 'TM6 movement'}
+    
+    for i,feature in enumerate(features):
+        for j in range(bt_numbers):
+            for k in range(2):
+                fraction_condition[i,j,k] = bt_condition_probability(feature,j,k) 
+    
+    pickle.dump(fraction_condition,open('./CB2_'+condition+'.pkl','wb'))
